0ed21b9b0b9d93f9d65592dcfc9a0ecafdc1d950
aisav <artur.isaverdyan@gmail.com>
2019-12-02 08:37:03 +0400

    Initial commit

---
+++ src/sagas/saga.js
@@ -0,0 +1,16 @@
+// import {delay} from 'redux-saga/effects'
+import {takeEvery, takeLatest, call, put}from 'redux-saga/effects'
+
+
+function* ageUpAsync() {
+  // yield delay(4000)
+  yield put({type: 'AGE_UP_ASYNC', value: 1})
+}
+
+
+
+
+export function* watchAgeUp(){
+  yield takeEvery('AGE_UP', ageUpAsync)
+  // yield takeLatest('AGE_UP', ageUpAsync)
+}
